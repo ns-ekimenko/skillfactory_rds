bade5ae35a86a3bf527baa9a2802b98c54cbce0e
nikolay <ns.ekimenko@vostokbunker.ru>
2020-04-17 23:33:14 +1000

    добавил комментарии к функции game_core_v4.\n Исправил ошибку: первое предсказание сделал 50 (вместо randing), при поиске сделал hi-1, lo+1 - т.к. эти значения не попадут уже в угаданные, начал count с 1, т.к. 50 это уже первая попытка.

--- module_0/main.py
+++ module_0/main.py
@@ -2,27 +2,43 @@ import numpy as np
 
 def score_game(game_core_v1):
     '''Запускаем игру 1000 раз, чтобы узнать, как быстро игра угадывает число'''
+    
     count_ls = []
     np.random.seed(1)  # фиксируем RANDOM SEED, чтобы ваш эксперимент был воспроизводим!
     random_array = np.random.randint(1, 101, size=(1000))
+    
     for number in random_array:
         count_ls.append(game_core_v1(number))
+    
     score = int(np.mean(count_ls))
     print(f"Ваш алгоритм угадывает число в среднем за {score} попыток")
+    
     return(score)
 
 def game_core_v4(number):
-    
-    count = 0
-    lo,hi = 1,101
-    predict = np.random.randint(lo,hi)
+    '''
+    Функция играет в угадайку. 
+    Пытается угадать переданное число от 0 до 100. 
+    Алгоритм поиска - каждый раз сужаем область поиска - основываясь на том,
+    что загаданное число больше или меньше предсказанного.
+    Возвращает количество попыток, за которое удалось угадать.
+    Параметры:
+        number - загаданное число
+    '''
+    count = 1
+    lo,hi = 1,100 # нижняя и верхняя граница области поиска
+    predict = 50
+
     while number != predict:
         count+=1
+        
+        # уменьшаем область поиска
         if number > predict: 
-            lo = predict
+            lo = predict+1
         elif number < predict: 
-            hi = predict
-        predict = lo + (hi-lo)//2
+            hi = predict-1
+        
+        predict = lo + (hi-lo)//2 # берем середину от новой области поиска
         
     return count # выход из цикла, если угадали
 
