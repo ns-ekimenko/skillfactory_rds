b4408f21e019226bd9b7cc2bce8f1061608cbb19
nikolay <ns.ekimenko@vostokbunker.ru>
2020-04-18 00:29:12 +1000

    format file using pycodestyle

--- module_0/main.py
+++ module_0/main.py
@@ -1,24 +1,29 @@
 import numpy as np
 
+
 def score_game(game_core_v1):
-    '''Запускаем игру 1000 раз, чтобы узнать, как быстро игра угадывает число'''
-    
+    '''
+    Запускаем игру 1000 раз, чтобы узнать, как быстро игра угадывает число
+    '''
+    # фиксируем RANDOM SEED, чтобы ваш эксперимент был воспроизводим!
+    np.random.seed(1)
+
     count_ls = []
-    np.random.seed(1)  # фиксируем RANDOM SEED, чтобы ваш эксперимент был воспроизводим!
     random_array = np.random.randint(1, 101, size=(1000))
-    
+
     for number in random_array:
         count_ls.append(game_core_v1(number))
-    
+
     score = int(np.mean(count_ls))
     print(f"Ваш алгоритм угадывает число в среднем за {score} попыток")
-    
+
     return(score)
+
 
 def game_core_v4(number):
     '''
-    Функция играет в угадайку. 
-    Пытается угадать переданное число от 0 до 100. 
+    Функция играет в угадайку.
+    Пытается угадать переданное число от 0 до 100.
     Алгоритм поиска - каждый раз сужаем область поиска - основываясь на том,
     что загаданное число больше или меньше предсказанного.
     Возвращает количество попыток, за которое удалось угадать.
@@ -26,23 +31,23 @@ def game_core_v4(number):
         number - загаданное число
     '''
     count = 1
-    lo,hi = 1,100 # нижняя и верхняя граница области поиска
+    lo, hi = 1, 100  # нижняя и верхняя граница области поиска
     predict = 50
 
     while number != predict:
-        count+=1
-        
+        count += 1
+
         # уменьшаем область поиска
-        if number > predict: 
+        if number > predict:
             lo = predict+1
-        elif number < predict: 
+        elif number < predict:
             hi = predict-1
-        
-        predict = lo + (hi-lo)//2 # берем середину от новой области поиска
-        
-    return count # выход из цикла, если угадали
+
+        predict = lo + (hi-lo)//2  # берем середину от новой области поиска
+
+    return count  # выход из цикла, если угадали
+
 
 if __name__ == '__main__':
     # Проверяем
     score_game(game_core_v4)
-    
